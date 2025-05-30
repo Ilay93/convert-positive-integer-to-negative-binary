c4b712c1c3059079f4e1e0fe6c4dab72c6da9d89
Ilay Spector <ilaisp4@gmail.com>
2025-05-30 17:11:22 +0300

    add main and running

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ def convert_bin_to_negative(number: str) -> str:
     pass
 
 
+def main():
+    number = int(input("Enter a number: "))
+    bin_number = convert_bin_to_negative(number)
+    negative_bin_number = convert_bin_to_negative(bin_number)
+    print(negative_bin_number)
+
+
+if __name__ == "__main__":
+    main()
