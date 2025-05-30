15e9b92626ce32899bac9d0e82a75342d256ae3a
Ilay Spector <ilaisp4@gmail.com>
2025-05-30 18:01:17 +0300

    add convert_to_binary function, update main function(fixed error in calling and added the extra parameter)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(number:int) -> str:
-    pass
+def convert_to_binary(number: int, num_bits: int) -> str:
+    return bin(number)[2:].zfill(num_bits)
+
 
 def convert_bin_to_negative(number: str) -> str:
     pass
@@ -7,9 +8,12 @@ def convert_bin_to_negative(number: str) -> str:
 
 def main():
     number = int(input("Enter a number: "))
-    bin_number = convert_bin_to_negative(number)
+    bits_amount = int(input("Enter the amounts of bits: "))
+    bin_number = convert_to_binary(number, bits_amount)
+    print(bin_number)
     negative_bin_number = convert_bin_to_negative(bin_number)
     print(negative_bin_number)
+    
 
 
 if __name__ == "__main__":
