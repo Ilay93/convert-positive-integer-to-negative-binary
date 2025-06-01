f785721487c6e7a68883cf765eabfb1463d80e65
Ilay Spector <ilaisp4@gmail.com>
2025-06-01 15:27:32 +0300

    remove unused code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def convert_to_binary(number: int, num_bits: int) -> str:
-    binary_number = bin(number)[2:0]
     return bin(number)[2:].zfill(num_bits)
 
 
@@ -20,10 +19,11 @@ def main():
     number = int(input("Enter a number: "))
     if number <= 0:
         raise Exception("only positive numbers can be entered")
+    
     bits_amount = int(input("Enter the amounts of bits: "))
     bin_number = convert_to_binary(number, bits_amount)
-    print(bin_number)
     negative_bin_number = convert_bin_to_negative(bin_number)
+
     print(negative_bin_number)
     
 
